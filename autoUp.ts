0aa6e64d550a94b6b3c1c61503b3a48879d59786
kita127 <kitada.kosuke@gmail.com>
2023-05-12 00:58:01 +0900

    up: POSTのサンプル作成

--- autoUp.ts
+++ autoUp.ts
@@ -1,35 +1,82 @@
 import axios from 'axios';
-
-// https://kita127.hatenablog.com/entry/2023/01/08/000741
+//import xml2js from 'xml2js';
+//import Base64 from 'Base64';
+import { Buffer } from 'buffer';
 
 const userId = 'kita127';
 const blogId = 'kita127.hatenablog.com';
 const url = `https://blog.hatena.ne.jp/${userId}/${blogId}/atom/entry`;
 const username = 'kita127';
 const apiKey = 'sgzt3btztd';
+const basicAuth = 'Basic ' + btoa(userId + ':' + apiKey);
 
 //GET https://blog.hatena.ne.jp/{はてなID}/{ブログID}/atom/entry
 
 //const entryId = '取得する記事のID';
 //const apiUrl = `https://blog.hatena.ne.jp/${userId}/${blogId}/atom/entry/${entryId}`;
 
-axios.get(url, {
-    headers: {
-        'Content-Type': 'application/xml',
-    },
-    auth: {
-        username: username,
-        password: apiKey,
-    },
-}).then(response => {
-    const xmlString = response.data;
-    console.log(xmlString);
-    // const parser = new DOMParser();
-    // const xmlDoc = parser.parseFromString(xmlString, 'application/xml');
-    // const title = xmlDoc.getElementsByTagName('title')[0].textContent;
-    // const content = xmlDoc.getElementsByTagName('content')[0].textContent;
-    // console.log(`Title: ${title}`);
-    // console.log(`Content: ${content}`);
-}).catch(error => {
-    console.error(error);
-});
+//get();
+post();
+
+async function post(): Promise<void> {
+    // リクエストのXMLデータを構築
+    const contents = `# タイトル
+## その1
+
+本文本文本文
+本文本文本文
+
+## その2
+
+本文
+`
+
+    const xmlData = `<?xml version="1.0" encoding="utf-8"?>
+    <entry xmlns="http://www.w3.org/2005/Atom">
+      <title>新規タイトル</title>
+      <content>${contents}</content>
+      <updated>${new Date().toISOString()}</updated>
+    </entry>`;
+
+    // Authorizationヘッダに含める認証情報をBase64エンコード
+    const encodedApiKey = Buffer.from(`${userId}:${apiKey}`).toString('base64');
+
+    // POSTリクエストを送信
+    const response = await axios.post(url, xmlData, {
+        headers: {
+            'Authorization': `Basic ${encodedApiKey}`,
+            'Content-Type': 'application/xml',
+        },
+    });
+
+    // HTTPステータスコードが201 Createdであれば、正常に処理されたと判断
+    if (response.status === 201) {
+        console.log('投稿が正常に処理されました。');
+    } else {
+        console.error(`HTTPステータスコード ${response.status} が返されました。`);
+    }
+}
+
+function get(): void {
+    axios.get(url, {
+        headers: {
+            'Content-Type': 'application/xml',
+        },
+        auth: {
+            username: username,
+            password: apiKey,
+        },
+    }).then(response => {
+        const xmlString = response.data;
+        console.log(xmlString);
+        // const parser = new DOMParser();
+        // const xmlDoc = parser.parseFromString(xmlString, 'application/xml');
+        // const title = xmlDoc.getElementsByTagName('title')[0].textContent;
+        // const content = xmlDoc.getElementsByTagName('content')[0].textContent;
+        // console.log(`Title: ${title}`);
+        // console.log(`Content: ${content}`);
+    }).catch(error => {
+        console.error(error);
+    });
+}
+
