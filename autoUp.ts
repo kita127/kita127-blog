e8d8bb252de58f1f76293aca3a8f851d1408b63f
kita127 <kitada.kosuke@gmail.com>
2023-05-13 21:57:28 +0900

    up: entries.jsonから情報を取得するよう変更

--- autoUp.ts
+++ autoUp.ts
@@ -3,12 +3,25 @@ import axios from 'axios';
 //import Base64 from 'Base64';
 import { DOMParser } from 'xmldom';
 //import { Buffer } from 'buffer';
+import { format } from 'util';
+import * as fs from 'fs';
+import { exit } from 'process';
 
-const userId = 'kita127';
-const blogId = 'kita127.hatenablog.com';
-const URL = `https://blog.hatena.ne.jp/${userId}/${blogId}/atom/entry`;
-const apiKey = 'sgzt3btztd';
+//const userId = 'kita127';
+//const blogId = 'kita127.hatenablog.com';
+const URL = `https://blog.hatena.ne.jp/%s/%s/atom/entry`;
+//const apiKey = 'sgzt3btztd';
 const entryId = '4207575160648581415';
+
+interface Config {
+    userId: string;
+    blogId: string;
+    apiKey: string;
+    entries: {
+        title: string;
+        srcPath: string;
+    }[];
+}
 
 interface EntryInfo { title: string, code: string }
 
@@ -16,7 +29,13 @@ interface EntryInfo { title: string, code: string }
 main();
 
 function main(): void {
-    getEntriesInfo()
+    const config = readJsonFile("./entries.json");
+    if (config === null) {
+        console.error('entries.jsonのパースに失敗');
+        exit(1);
+    }
+
+    getEntriesInfo(config)
         .then((info: EntryInfo[]) => {
             console.log(info);
         })
@@ -29,81 +48,92 @@ function main(): void {
 
 //post();
 
-async function put(): Promise<void> {
-    // 更新するためのXMLデータを作成
-    const contents = `更新後コンテンツ`;
-    const xmlData = `<?xml version="1.0" encoding="utf-8"?>
-    <entry xmlns="http://www.w3.org/2005/Atom">
-      <id>${entryId}</id>
-      <title>更新後タイトル</title>
-      <content>${contents}</content>
-      <updated>${new Date().toISOString()}</updated>
-    </entry>`;
-
-    // 記事の更新
-    await axios.put(`${URL}/${entryId}`, xmlData, {
-        headers: {
-            'Content-Type': 'application/xml',
-        },
-        auth: {
-            username: userId,
-            password: apiKey,
-        },
-    });
-}
-
-async function post(): Promise<void> {
-    // リクエストのXMLデータを構築
-    const contents = `# タイトル
-## その1
-
-本文本文本文
-本文本文本文
-
-## その2
-
-本文
-`
-
-    const xmlData = `<?xml version="1.0" encoding="utf-8"?>
-    <entry xmlns="http://www.w3.org/2005/Atom">
-      <title>新規タイトル</title>
-      <content>${contents}</content>
-      <updated>${new Date().toISOString()}</updated>
-    </entry>`;
-
-    // POSTリクエストを送信
-    const response = await axios.post(URL, xmlData, {
-        headers: {
-            'Content-Type': 'application/xml',
-        },
-        auth: {
-            username: userId,
-            password: apiKey,
-        },
-    });
-
-    // HTTPステータスコードが201 Createdであれば、正常に処理されたと判断
-    if (response.status === 201) {
-        console.log('投稿が正常に処理されました。');
-    } else {
-        console.error(`HTTPステータスコード ${response.status} が返されました。`);
+function readJsonFile(filePath: string): Config | null {
+    try {
+        const data = fs.readFileSync(filePath, "utf8");
+        return JSON.parse(data) as Config;
+    } catch (error) {
+        console.error(`Failed to read JSON file: ${error}`);
+        return null;
     }
 }
 
-async function getEntriesInfo(): Promise<EntryInfo[]> {
+
+// async function put(): Promise<void> {
+//     // 更新するためのXMLデータを作成
+//     const contents = `更新後コンテンツ`;
+//     const xmlData = `<?xml version="1.0" encoding="utf-8"?>
+//     <entry xmlns="http://www.w3.org/2005/Atom">
+//       <id>${entryId}</id>
+//       <title>更新後タイトル</title>
+//       <content>${contents}</content>
+//       <updated>${new Date().toISOString()}</updated>
+//     </entry>`;
+
+//     // 記事の更新
+//     await axios.put(`${URL}/${entryId}`, xmlData, {
+//         headers: {
+//             'Content-Type': 'application/xml',
+//         },
+//         auth: {
+//             username: userId,
+//             password: apiKey,
+//         },
+//     });
+// }
+
+// async function post(): Promise<void> {
+//     // リクエストのXMLデータを構築
+//     const contents = `# タイトル
+// ## その1
+
+// 本文本文本文
+// 本文本文本文
+
+// ## その2
+
+// 本文
+// `
+
+//     const xmlData = `<?xml version="1.0" encoding="utf-8"?>
+//     <entry xmlns="http://www.w3.org/2005/Atom">
+//       <title>新規タイトル</title>
+//       <content>${contents}</content>
+//       <updated>${new Date().toISOString()}</updated>
+//     </entry>`;
+
+//     // POSTリクエストを送信
+//     const response = await axios.post(URL, xmlData, {
+//         headers: {
+//             'Content-Type': 'application/xml',
+//         },
+//         auth: {
+//             username: userId,
+//             password: apiKey,
+//         },
+//     });
+
+//     // HTTPステータスコードが201 Createdであれば、正常に処理されたと判断
+//     if (response.status === 201) {
+//         console.log('投稿が正常に処理されました。');
+//     } else {
+//         console.error(`HTTPステータスコード ${response.status} が返されました。`);
+//     }
+// }
+
+async function getEntriesInfo(config: Config): Promise<EntryInfo[]> {
     let titles: string[] = [];
     let cds: string[] = [];
 
-    let url: string | null = URL;
+    let url: string | null = format(URL, config.userId, config.blogId);
     while (url) {
         const response = await axios.get(url, {
             headers: {
                 'Content-Type': 'application/xml',
             },
             auth: {
-                username: userId,
-                password: apiKey,
+                username: config.userId,
+                password: config.apiKey,
             },
         })
 
